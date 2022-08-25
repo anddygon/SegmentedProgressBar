2ce82223396111451508673f6cff6a22a4a73019
xiaopeng <gongxiaopeng@xsmart.com>
2022-08-25 16:18:50 +0800

    initial commit

---
+++ SegmentedProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedProgressBar
+//
+//  Created by 巩小鹏 on 2022/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
